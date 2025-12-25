d3f2c957dbc9e15c4f7d697364a7f20adc4ff2ce
md-miyam <mdmiyam20@gmail.com>
2025-12-25 16:25:43 +0600

    textField validatin added

--- lib/Views/CreatePostScreen/create_post_screen.dart
+++ lib/Views/CreatePostScreen/create_post_screen.dart
@@ -8,6 +8,8 @@ class CreatePostScreen extends StatefulWidget {
   State<CreatePostScreen> createState() => _CreatePostScreenState();
 }
 
+final fromKey = GlobalKey<FormState>();
+
 TextEditingController titleController = TextEditingController();
 TextEditingController bodyController = TextEditingController();
 TextEditingController userIdController = TextEditingController();
@@ -28,57 +30,96 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: titleController,
-              decoration: InputDecoration(
-                hintText: "Title",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 15),
-              child: TextFormField(
-                controller: bodyController,
+        child: Form(
+          key: fromKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: titleController,
                 decoration: InputDecoration(
-                  hintText: "Body",
+                  hintText: "Title",
                   border: OutlineInputBorder(),
                 ),
+                validator: (v) {
+                  if (v!.isEmpty) {
+                    return "Title required";
+                  } else {
+                    return null;
+                  }
+                },
               ),
-            ),
-            TextFormField(
-              controller: userIdController,
-              decoration: InputDecoration(
-                hintText: "User id",
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            // button
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 15),
-              child: SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    ApiServices.postData(
-                      titleController.text,
-                      bodyController.text,
-                      userIdController.text,
-                    );
-                    controllerClean();
-                    Navigator.pop(context);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8)),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 15),
+                child: TextFormField(
+                  controller: bodyController,
+                  decoration: InputDecoration(
+                    hintText: "Body",
+                    border: OutlineInputBorder(),
                   ),
-                  child: Text("Create"),
+                  validator: (v) {
+                    if (v!.isEmpty) {
+                      return "Body required";
+                    } else {
+                      return null;
+                    }
+                  },
                 ),
               ),
-            ),
-          ],
+              TextFormField(
+                controller: userIdController,
+                decoration: InputDecoration(
+                  hintText: "User id",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (v) {
+                  if (v!.isEmpty) {
+                    return "User id required";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+
+              // button
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 15),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (fromKey.currentState!.validate()) {
+                        ApiServices.postData(
+                          titleController.text,
+                          bodyController.text,
+                          userIdController.text,
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              "Post created successfully! ${ApiServices.postData}",
+                            ),
+                            duration: Duration(
+                              seconds: 3,
+                            ), // how long it stays visible
+                          ),
+                        );
+                        controllerClean();
+                        Navigator.pop(context);
+                      } else {
+                        return;
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: Text("Create"),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
